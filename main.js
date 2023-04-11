3ad97b5beea2228549546ec809f099ef663bf61c
Norbekov214 <norbekov.asilbek288@gmail.com>
2023-04-11 20:44:34 +0500

    push

---
+++ main.js
@@ -0,0 +1,35 @@
+const btn = document.querySelectorAll(`li`);
+const mainItem = document.querySelector(`h2`);
+const image = document.querySelector(`img`);
+const content = document.querySelector(".content");
+image.addEventListener(`click`, (e) => {
+    content.classList.toggle("show");
+})
+
+btn.forEach((li) => {
+    li.addEventListener('click', (e) => {
+        const value = e.target.textContent;
+        console.log(value);
+        switch (value) {
+            case "c":
+                mainItem.textContent = " ";
+                break;
+            case "del":
+                mainItem.textContent = mainItem.textContent.slice(0, -1);
+                break;
+            case "=":
+                try {
+                    mainItem.textContent = eval(mainItem.textContent)
+                } catch (err) {
+                    mainItem.textContent = "You must learn to write correctly"
+                }
+                break
+            default:
+                mainItem.textContent += value;
+
+        }
+
+    })
+})
+
+
